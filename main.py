32ae4af9f07a341d3661120be8083f2874a43dd3
edudevcf <cforan99@gmail.com>
2026-09-02 23:56:35 -0700

    hooked up openrouter api and got responses to my first client chat

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("could not find API key in .env")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
