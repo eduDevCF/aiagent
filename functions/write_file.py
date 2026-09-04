f06f9a4561f07400baacd365ad27093fd084318e
edudevcf <cforan99@gmail.com>
2026-09-03 22:12:10 -0700

    added functions and tests to write file content to a valid file path

---
+++ functions/write_file.py
@@ -0,0 +1,32 @@
+import os
+from functions.get_files_info import validate_path
+from functions.get_file_content import get_file_content
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        path_is_valid, target_path = validate_path(working_directory, file_path)
+        
+        if os.path.isdir(target_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+        if not path_is_valid:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        else:
+            # make all parent directories if they don't exist
+            os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+            # write to file path
+            with open(target_path, "w") as f:
+                f.write(content)
+            
+            # read file to check if it wrote all the content
+            try:
+                written_content = get_file_content(working_directory, target_path)
+                # just checking for now
+                print(content)
+                if len(written_content) == len(content):
+                    return f'Successfully wrote to "{target_path}" ({len(content)} characters written)'
+            except Exception as e:
+                return f"Error: Something went wrong when confirming that we wrote to the file: {e}"
+    except Exception as e:
+        return f"Error: Something went wrong trying to write to '{file_path}': {e}"
