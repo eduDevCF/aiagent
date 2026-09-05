bdf51086f90ccba60fae710f795fc238be9896c9
edudevcf <cforan99@gmail.com>
2026-09-04 19:43:44 -0700

    made a couple tweaks after reviewing the solution files to this point

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,7 +4,8 @@ from functions.get_files_info import validate_path
 def run_python_file(working_directory: str, file_path: str, args: list=[]) -> str:
     try:
         path_is_valid, abs_path = validate_path(working_directory, file_path)
-        
+        abs_working_dir = os.path.abspath(working_directory)
+
         if not os.path.isfile(abs_path):
             return f'Error: "{file_path}" does not exist or is not a regular file'
         if not file_path.endswith(".py"):
@@ -14,17 +15,24 @@ def run_python_file(working_directory: str, file_path: str, args: list=[]) -> st
         else:
             # build the command
             command = ["python", abs_path]
-            command.extend(args)
+            if args:
+                command.extend(args)
             # run the command in a subprocess
-            completed_process = subprocess.run(command, capture_output=True, text=True, timeout=30)
+            completed_process = subprocess.run(
+                command, 
+                cwd=abs_working_dir,
+                capture_output=True, 
+                text=True, 
+                timeout=30)
             # build output string
             output = ""
             if completed_process.returncode != 0:
                 output += f"Error: Process exited with code {completed_process.returncode}\n"
-            if completed_process.stdout is None and completed_process.stderr is None:
+            if not completed_process.stdout and not completed_process.stderr:
                 output += "No output produced\n"
-            else:
+            if completed_process.stdout:
                 output += f"STDOUT: {completed_process.stdout}\n"
+            if completed_process.stderr:
                 output += f"STDERR: {completed_process.stderr}\n"
             return output
 
